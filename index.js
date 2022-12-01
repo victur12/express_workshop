411afbc6512938923ba53d6d9cc93a6325e7fcbe
Victor González <vgonzalezvictor0@gmail.com>
2022-11-30 19:25:26 -0600

    Activity 14 Connect with UI

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const user = require('./routes/user');
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const index = require("./middleware/index");
-
+const cors = require("./middleware/cors");
 /*
 Verbos HTTP
 GET - obtener recursos
@@ -15,6 +15,7 @@ PUT - modificar un recurso
 PATCH - modificar un una parte de un recurso
 DELETE - borrar un recurso
 */
+app.use(cors);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
